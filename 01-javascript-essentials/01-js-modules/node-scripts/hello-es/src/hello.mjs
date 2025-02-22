b914e44b68e011820751ad0aa1849775512b90a0
Prasad Jayakumar <prasad.jayakumar@gmail.com>
2025-02-22 19:20:46 +0530

    Add greeting functionality with user input in ES and CommonJS modules

---
+++ 01-javascript-essentials/01-js-modules/node-scripts/hello-es/src/hello.mjs
@@ -0,0 +1,4 @@
+export function greet(name) {
+  // console.log(): Displays the greeting in the console.
+  console.log(`Hello, ${name}!`);
+}
--- 01-javascript-essentials/01-js-modules/node-scripts/hello-es/src/main.mjs
+++ 01-javascript-essentials/01-js-modules/node-scripts/hello-es/src/main.mjs
@@ -1,6 +1,6 @@
 import readline from 'readline';
+import { greet } from './hello.mjs';
 
-// readline: A built-in Node.js module for reading input from the console.
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -8,8 +8,7 @@ const rl = readline.createInterface({
 
 // readline.question(): Prompts the user for input and executes a callback function when the input is received.
 rl.question('Enter your name: ', name => {
-    // console.log(): Displays the greeting in the console.
-    console.log(`Hello, ${name}!`);
+    greet(name);
 
     // readline.close(): Closes the input stream.
     rl.close();
