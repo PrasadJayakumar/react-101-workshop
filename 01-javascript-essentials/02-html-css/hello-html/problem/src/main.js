7f909cfd4ee5ed07417eb97bf186514ae56f62d6
Prasad Jayakumar <prasad.jayakumar@gmail.com>
2025-02-23 16:25:33 +0530

    Add user input functionality with textbox and button in HTML structure

--- 01-javascript-essentials/02-html-css/hello-html/problem/src/main.js
+++ 01-javascript-essentials/02-html-css/hello-html/problem/src/main.js
@@ -1,9 +1,6 @@
 import { greet } from './hello.js';
 
 function getNameAndGreet() {
-    // Create a textbox for the user to enter their name
-    // Create a button for the user to click
-
     // When the button is clicked, call the greet function
     greet();
 }
--- 01-javascript-essentials/02-html-css/hello-html/solution/src/main.js
+++ 01-javascript-essentials/02-html-css/hello-html/solution/src/main.js
@@ -1,10 +1,6 @@
 import { greet } from './hello.js';
 
 function getNameAndGreet() {
-    // Create a textbox for the user to enter their name
-    // Create a button for the user to click
-    // ???
-
     // When the button is clicked, call the greet function
     document.getElementById('Submit').addEventListener('click', function () {
         const name = document.getElementById('name').value;
