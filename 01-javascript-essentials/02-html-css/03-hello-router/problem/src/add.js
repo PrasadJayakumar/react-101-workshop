234c977f5720dcdc1aec0718ade04c063a8cac55
Prasad Jayakumar <prasad.jayakumar@gmail.com>
2025-02-23 22:29:48 +0530

    Add renderAddContainer function to dynamically create input fields and display sum from query string

---
+++ 01-javascript-essentials/02-html-css/03-hello-router/problem/src/add.js
@@ -0,0 +1,4 @@
+export function renderAddContainer() {
+    //TODO: Create three textboxes and a button dynamically using JavaScript.
+    //TODO: Accept a and b from query string and show the sum in the third textbox on load.
+}
--- 01-javascript-essentials/02-html-css/03-hello-router/problem/src/main.js
+++ 01-javascript-essentials/02-html-css/03-hello-router/problem/src/main.js
@@ -1,6 +1,8 @@
 import { getNameAndGreet } from './hello.js';
+import { renderAddContainer } from './add.js';
 
 // Ensure the DOM is fully loaded before adding the event listener
 document.addEventListener('DOMContentLoaded', function () {
     getNameAndGreet();
+    add();
 });
